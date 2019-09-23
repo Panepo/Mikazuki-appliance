b1fa446f4f77a9c3a0dad6f403578e87740323c1
Panepo <whisper.ko@gmail.com>
2019-09-23 11:59:58 +0800

    update server
    remove logger

--- server/src/server.js
+++ server/src/server.js
@@ -3,29 +3,20 @@ import 'regenerator-runtime/runtime'
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
-import dayjs from 'dayjs'
-import 'dayjs/locale/zh-tw'
-
 import textRoutes from './routes/text.route'
-
-import logger from './services/logger.service'
 
 const app = express()
 
-// Days locale
-dayjs.locale('zh-tw')
-
-// MIDDLEWARES
+// middlewares
 if (process.env.NODE_ENV !== 'production') app.use(cors())
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(bodyParser.json({ limit: '50mb', extended: true }))
 
-// ROUTES
+// routes
 app.use('/text', textRoutes)
 
-// DATA ROUTE
-// app.use('/data', express.static(dataFolder))
+// static web serving
+app.use(express.static('./static'))
 
 // By using (!module.parent) condition, we avoid EADDRINUSE when testing because app will start once
 let startApp = process.env.NODE_ENV != 'test'
@@ -34,13 +25,13 @@ if (process.env.NODE_ENV == 'test' && !module.parent) startApp = true
 if (startApp) {
   app.listen(3001, () => {
     if (!process.env.NODE_ENV) {
-      logger.error(`> No environment !! Server will not start`)
+      console.error(`[ERROR] > No environment !! Server will not start`)
       process.exit(0)
       return
     }
 
-    logger.info('> Listening on port 3001...')
-    logger.info(`> Current environment is => ${process.env.NODE_ENV}`)
+    console.info('[INFO] > Listening on port 3001...')
+    console.info(`[INFO] > Current environment is => ${process.env.NODE_ENV}`)
   })
 }
 
